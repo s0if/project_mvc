4fbc2d55838ea6cbe761149f7d278cd2092c21bf
s0if <saifalkomi@gmail.com>
2024-10-04 22:03:09 +0300

    initoal dashbord

---
+++ Landing.PL/Areas/Dashbord/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Landing.PL.Areas.Dashbord.Controllers
+{
+    [Area("Dashbord")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult add()
+        {
+            return View();
+        }
+    }
+}
